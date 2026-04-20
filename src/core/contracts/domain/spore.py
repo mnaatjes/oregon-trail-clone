a09afa7e26ae67db7c08dabfbe2b6ae07b555db8
mnaatjes <michael.naatjes87@gmail.com>
2026-04-20 02:44:11 +0000

    ref-docs: ADR-015

--- src/core/contracts/domain/spore.py
+++ src/core/contracts/domain/spore.py
@@ -1,4 +1,4 @@
-# src/core/contracts/value_object.py
+# src/core/contracts/domain/spore.py
 
 from abc import ABC
 from dataclasses import dataclass
@@ -7,32 +7,30 @@ from typing import get_type_hints
 @dataclass(frozen=True)
 class DomainSpore(ABC):
     """
-    Abstract base for all Domain Value Objects (DomainFamily.SPORE).
+    Abstract base for all Domain Spores (DomainFamily.SPORE).
     
-    - Value Objects have no identity (UID). 
+    - Spores have no identity (UID). 
         - Two instances are considered identical if all their attributes are equal.
-        - Data IS the ID
+        - Data IS the ID (Value Equality).
 
     - Attributes are immutable (frozen) to ensure thread-safety and 
     domain integrity.
 
-    - Specific Data Bundle unique to one instance but performs no action
-        - e.g. CharacterIdentity("Jedediah")
-        - Collections of Blueprints or values or both
-        - Immutable State
-        - Created in Logic
-        - Saved to Storage
+    - Specific Data Bundle unique to one instance but performs no action.
+        - e.g. Coordinates(x=10, y=20)
+        - Collections of Blueprints or values or both.
+        - Immutable State used for semantic typing.
     """
     def __init_subclass__(cls, **kwargs) -> None:
         super().__init_subclass__(**kwargs)
-        # Identity Purity Guard
+        # Identity Purity Guard: Ensures Spores don't accidentally gain identity
         forbidden = {"uid", "uuid", "id", "slug"}
         for f in get_type_hints(cls):
             if f.lower() in forbidden:
                 raise TypeError(
                     f"[IDENTITY VIOLATION] in '{cls.__name__}' "
                     f"CANNOT use property '{f}' "
-                    f"Use of Identification in DomainValueObjects PROHIBITED!"
+                    f"Use of Identification in DomainSpores PROHIBITED!"
                 )
 
     def __post_init__(self) -> None:
@@ -41,7 +39,7 @@ class DomainSpore(ABC):
     
     def validate(self) -> None:
         """
-        Override this in subclasses to enforce rules.
+        Override this in subclasses to enforce business rules.
         Should raise ValueError or TypeError if invalid.
         """
         pass
